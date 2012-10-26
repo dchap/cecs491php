28c93a3b351da2d61c207726cef1d28977b68642
yohei <yohei.takayama@student.csulb.edu>
2012-10-25 19:24:21 -0700

    10/25/2012/19:25
    idk...

--- sharktopus/config/database/mysqli_connection.php
+++ sharktopus/config/database/mysqli_connection.php
@@ -3,10 +3,13 @@ namespace Config\Database
 {
     class Mysqli_Connection
     {
+        //private static $dbuser = "user";             //user name for the database
+        //private static $dbpass = "password"; 
         private static $dbuser = "csulbsha_shark";             //user name for the database
         private static $dbpass = "acoustictelemetry";      //the password to access the database
         private static $dbname = "csulbsha_sharktopus";             //the name of the database
         private static $dbhost = "localhost";
+        //private static $dbhost = "www.csulbsharklab.com";
 
         private static $db = null;
         
@@ -15,13 +18,20 @@ namespace Config\Database
         }
         
         public static function GetMysqliInstance()
-        {
+        {   
+/*
+            self::$db = mysql_connect($dbhost, $dbuser , $dbpass);
+            if (!self::$db)
+            {
+                die('Could not connect: ' . mysql_error());
+            }
+*/
+            
             if (!(self::$db instanceof \mysqli))
             {
                 self::$db = new \mysqli(self::$dbhost, self::$dbuser, self::$dbpass, self::$dbname)
                     or die("Could not connect to database: " . $db->connect_error);
             }
-            
             return self::$db;
         }
         
