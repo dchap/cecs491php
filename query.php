d4346ea63df9e74209049f987de0860bbc7c196d
Derek Chapman <Chapfactor88@verizon.net>
2012-11-01 13:55:46 -0700

    query constants

---
+++ query.php
@@ -0,0 +1,52 @@
+<?php
+namespace Config\Constants
+{
+    class Query
+    {
+        // keys
+        const Project = "project";
+        const OuterQueryType = "outer-query";
+        const InnerQueryType = "inner-query";
+        const Fields = "fields";
+        const SortBy = "sort-by";
+        const SortOrder = "sort-order";
+        const Limit = "limit";
+        const Count = "count";
+        const Query = "query";
+        const Page = "page";
+        const DateStart = "date-start";
+        const DateEnd = "date-end";
+        const TimeStart = "time-start";
+        const TimeEnd = "time-end";
+        const StationKey = "stations_name";
+        const Receiver = "receivers_id";
+        const TransmitterRangeStart = "transmitter_start";
+        const TransmitterRangeEnd = "transmitter_end";
+        const Genus = "genus";
+        const Species = "species";
+        const FrequencyCodespace = "frequency_codespace";
+        //const TransmitterID = "transmitterid";
+        const TransmitterID = "transmitter_id";
+        const Weeks = 150;
+        const Days = 7;
+        const Hours = 24;
+        const Minutes = 60;
+        const Seconds = 60;
+        
+        
+        // uploads keys
+        const Filetypes = "filetypes";
+        const Members = "members";
+        const Filename = "filename";
+        
+        // values
+        const Fish = "fish";
+        const Station = "station";
+        const Transmitter = "transmitter";
+        const Sensor = "sensor";
+        const RecognizedFish = "recognized-fish";
+        const UnrecognizedFish = "unrecognized-fish";
+    }
+
+}
+?>
